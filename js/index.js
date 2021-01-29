51c44c1cc7fc9e018d04c00268bd2dd46df12d8f
vophuocminhhao1997 <vophuocminhhao1997@gmail.com>
2021-01-29 13:30:05 +0700

    updat1

--- js/index.js
+++ js/index.js
@@ -1,21 +1,153 @@
-$(function () {
-    $('.gallery-container ul').isotope({
-      itemSelector: 'li'
-    })
-
-    // code cho phan nut
-    $('.gallery-filters ul li a').click(function (e) { 
-        // console.log("da click");
-      e.preventDefault();
-    //   console.log(this);
-      var room = $(this).data('room');
-      
-      if(room === 'classic'){
-        $(".gallery-container ul").isotope({ filter: '*' });
-      } else{
-        room = "." + room;
-        $(".gallery-container ul").isotope({ filter: room });
-      }
-      
-    });
+//Swiper Slides
+var swiperSlides = new Swiper(".swiper-slides", {
+  spaceBetween: 30,
+  effect: "fade",
+  autoplay: {
+    delay: 5000,
+  },
+  navigation: {
+    nextEl: ".button-slide-next",
+    prevEl: ".button-slide-prev",
+  },
 });
+//end
+
+// code isotop of Room
+// var $grid = jQuery(".room-container ul").isotope({
+//   filter: ".Classic",
+//   layoutMode: 'fitRows',
+// });
+
+// // code cho phan nut
+// $(".gallery-filters ul li a").click(function (e) {
+//   e.preventDefault();
+//   var room = $(this).data("room");
+//   let index = $(this).attr('data-id');
+//   var $grid = jQuery(".room-container ul").isotope({
+//     filter: "."+ index,
+//     layoutMode: 'fitRows',
+//   });
+// })
+
+$(function () {
+  $(".gallery-container ul").isotope({
+    itemSelector: "li",
+  });
+
+  // code cho phan nut
+  $(".gallery-filters ul li a").click(function (e) {
+    e.preventDefault();
+    var list = $(this).data("id");
+
+    if (list === "Classic") {
+      $(".gallery-container ul").isotope({ filter: "*" });
+    } else {
+      list = "." + list;
+      $(".gallery-container ul").isotope({ filter: list });
+    }
+  });
+});
+///end
+
+// Code swiper destination
+var swiperDestination = new Swiper(".swiper-desti", {
+  slidesPerView: 3,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  speed: 2000,
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+});
+//end
+
+//code lightGallery
+lightGallery(document.getElementById("aniimated-thumbnials"), {
+  thumbnail: true,
+  animateThumb: false,
+  showThumbByDefault: false,
+});
+//end
+
+// popup js
+// let slt = document.querySelectorAll(".gallery-container ul li");
+// let popup = document.querySelector(".room .pop-up");
+// let close = document.querySelector(".pop-up .close");
+// slt.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   popup.classList.add("active");
+// });
+// for (var i = 0, c = slt.length; i < c; i++) {
+//   slt[i].addEventListener("click", function (e) {
+//     e.preventDefault();
+//     if (e.target.tagName == "A") {
+//       popup.classList.add("active");
+//     }
+//   });
+// }
+
+// close.addEventListener("click", function () {
+//   popup.classList.remove("active");
+// });
+
+// popup jQuery
+$(".gallery-container ul li a.btn-2").on("click", function () {
+  $(".room .pop-up").fadeIn();
+  var galleryThumbs = new Swiper(".gallery-thumbs", {
+    slidesPerView: 4,
+    spaceBetween: 5,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  var galleryTop = new Swiper(".gallery-top", {
+    loop: true,
+    thumbs: {
+      swiper: galleryThumbs,
+    },
+  });
+  return false;
+});
+$(".pop-up .close").on("click", function () {
+  $(".room .pop-up").fadeOut();
+  return false;
+});
+//end
+
+//Code Swiper review
+var swiperReview = new Swiper(".swiper-review", {
+  slidesPerView: 3,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  speed: 2000,
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
+//end
