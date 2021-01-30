733d5fbfc6ebae594f107072a2231e221c25b74a
vophuocminhhao1997 <vophuocminhhao1997@gmail.com>
2021-01-30 14:27:47 +0700

    add back to top

--- js/index.js
+++ js/index.js
@@ -160,3 +160,20 @@ var swiperReview = new Swiper(".swiper-review", {
   },
 });
 //end
+
+
+// back to top
+let btn = $('.back-to-top');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
